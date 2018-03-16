bc12884771c61d4cccf7dc0c4d29ba72db8dab93
Simon <sdanisch@gmail.com>
2018-03-16 20:38:06 +0100

    remove tmp

--- docs/make.jl
+++ docs/make.jl
@@ -42,33 +42,3 @@ deploydocs(
     osname = "linux",
     make = nothing
 )
-
-function sortperm_int_range2(a, rangelen, minval)
-    cnts = fill(0, rangelen)
-
-    # create a count first
-    @inbounds for ai in a
-        cnts[ai - minval + 1] +=  1
-    end
-
-    # create cumulative sum
-    cumsum!(cnts, cnts)
-end
-    la = length(a)
-    res = Vector{Int}(la)
-    @simd for i in la:-1:1
-        @inbounds begin
-            ai = a[i] - minval + 1
-            c = cnts[ai]
-            cnts[ai] -= 1
-            res[c] = i
-        end
-    end
-    res
-end
-
-# to test the code
-rangelen = 1_000_000
-minval = 1
-a = rand(1:rangelen, 100_000_000)
-@time x = sortperm_int_range2(a, rangelen, 1); # 20 seconds; TOO SLOW
